4c4efdf220f60fa2a189ac28b16787b6e133f208
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-12-31 17:29:14 +0200

    UI works

--- IgniteAspireApp/IgniteAspireApp.Web/IgniteApiClient.cs
+++ IgniteAspireApp/IgniteAspireApp.Web/IgniteApiClient.cs
@@ -5,7 +5,7 @@ public class IgniteApiClient(HttpClient httpClient)
     public async Task<List<IgniteNode>> GetNodesAsync(int maxItems = 10, CancellationToken cancellationToken = default)
     {
         List<IgniteNode?> igniteNodes = await httpClient
-            .GetFromJsonAsAsyncEnumerable<IgniteNode>("/weatherforecast", cancellationToken)
+            .GetFromJsonAsAsyncEnumerable<IgniteNode>("/nodes", cancellationToken)
             .ToListAsync(cancellationToken: cancellationToken);
 
         return igniteNodes!;
