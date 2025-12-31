8b2edefa27ac3c12d50c2d9ada1d3f7af7c52858
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-12-31 16:15:17 +0200

    Add config mount

--- IgniteAspireApp/IgniteAspireApp.AppHost/AppHost.cs
+++ IgniteAspireApp/IgniteAspireApp.AppHost/AppHost.cs
@@ -11,10 +11,10 @@ builder.AddProject<Projects.IgniteAspireApp_Web>("webfrontend")
     .WithReference(apiService)
     .WaitFor(apiService);
 
-// TODO: WithVolume?
 builder.AddContainer("apacheignite", "apacheignite/ignite:3.1.0")
     .WithContainerName("ignite")
     .WithEndpoint(targetPort: 10300, port: 10300, name: "ignite-rest-api", scheme: "http")
-    .WithEndpoint(targetPort: 10800, port: 10800, name: "ignite-client", scheme: "tcp", protocol: ProtocolType.Tcp);
+    .WithEndpoint(targetPort: 10800, port: 10800, name: "ignite-client", scheme: "tcp", protocol: ProtocolType.Tcp)
+    .WithBindMount("./ignite/node-config.json", "/opt/ignite/etc/node-config.json");
 
 builder.Build().Run();
