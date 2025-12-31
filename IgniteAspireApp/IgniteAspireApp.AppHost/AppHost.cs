19a0cb686a55dfcf2cf7b338f19b4d9674748800
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-12-31 16:12:53 +0200

    wip integrate Ignite

--- IgniteAspireApp/IgniteAspireApp.AppHost/AppHost.cs
+++ IgniteAspireApp/IgniteAspireApp.AppHost/AppHost.cs
@@ -1,3 +1,5 @@
+using System.Net.Sockets;
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.IgniteAspireApp_ApiService>("apiservice")
@@ -8,5 +10,11 @@ builder.AddProject<Projects.IgniteAspireApp_Web>("webfrontend")
     .WithHttpHealthCheck("/health")
     .WithReference(apiService)
     .WaitFor(apiService);
+
+// TODO: WithVolume?
+builder.AddContainer("apacheignite", "apacheignite/ignite:3.1.0")
+    .WithContainerName("ignite")
+    .WithEndpoint(targetPort: 10300, port: 10300, name: "ignite-rest-api", scheme: "http")
+    .WithEndpoint(targetPort: 10800, port: 10800, name: "ignite-client", scheme: "tcp", protocol: ProtocolType.Tcp);
 
 builder.Build().Run();
