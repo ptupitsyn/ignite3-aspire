4d3c7bff9533f93da3bf0333400fe3fa77552750
Pavel Tupitsyn <ptupitsyn@apache.org>
2025-12-31 16:49:47 +0200

    wip init cluster

--- IgniteAspireApp/IgniteAspireApp.AppHost/AppHost.cs
+++ IgniteAspireApp/IgniteAspireApp.AppHost/AppHost.cs
@@ -11,11 +11,22 @@ builder.AddProject<Projects.IgniteAspireApp_Web>("webfrontend")
     .WithReference(apiService)
     .WaitFor(apiService);
 
-builder.AddContainer("apacheignite", "apacheignite/ignite:3.1.0")
+var igniteService = builder.AddContainer("apacheignite", "apacheignite/ignite:3.1.0")
     .WithContainerName("ignite")
     .WithEndpoint(targetPort: 10300, port: 10300, name: "ignite-rest-api", scheme: "http")
     .WithEndpoint(targetPort: 10800, port: 10800, name: "ignite-client", scheme: "tcp", protocol: ProtocolType.Tcp)
     .WithBindMount("./ignite/node-config.json", "/opt/ignite/etc/node-config.json")
-    .WithHttpHealthCheck("/", 200, "ignite-rest-api");
+    .WithHttpHealthCheck("/", 409, "ignite-rest-api");
+
+const string curlCmd = """
+                       curl -i --request POST --header "Content-Type: application/json" \
+                       --data '{"metaStorageNodes": ["defaultNode"], "clusterName": "myCluster"}' \ 
+                       http://apacheignite:10300/management/v1/cluster/init
+                       """;
+
+builder.AddContainer("apacheignite-init", "curlimages/curl")
+    .WaitFor(igniteService)
+    .WithArgs("sh", "-c", curlCmd)
+    .WithLifetime(ContainerLifetime.Session);
 
 builder.Build().Run();
